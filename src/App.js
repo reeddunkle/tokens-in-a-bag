1986f1fe6d9290e7c46b014f3ff0a953d2494eb1
Reed <reeddunkle@gmail.com>
2020-09-06 13:29:26 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,99 @@
-import React from "react";
-import "./styles.css";
+import range from 'lodash.range';
+import random from 'lodash.random';
+import React, { useState } from 'react';
+import './styles.css';
+
+const players = ['red', 'green'];
+
+const startingTokens = {
+  neutral: 0,
+  black: 0,
+  ...players.reduce(
+    (acc, color) => ({
+      ...acc,
+      [color]: 0
+    }),
+    {}
+  )
+};
+
+const filterValue = (obj, value) => {
+  return Object.entries(obj).reduce((acc, [key, val]) => {
+    return val === value
+      ? acc
+      : {
+          ...acc,
+          [key]: val
+        };
+  }, {});
+};
 
 export default function App() {
+  const [tokens, setTokens] = useState(startingTokens);
+  const [drawn, setDrawn] = useState();
+
+  const addToken = (key) => {
+    setTokens({
+      ...tokens,
+      [key]: tokens[key] + 1
+    });
+  };
+
+  const subtractToken = (key) => {
+    setTokens({
+      ...tokens,
+      [key]: tokens[key] - 1
+    });
+  };
+
+  const drawRandom = () => {
+    const bag = Object.entries(tokens).reduce((acc, [key, value]) => {
+      return [...acc, ...range(value).map(() => key)];
+    }, []);
+
+    if (bag.length === 0) {
+      return;
+    }
+
+    const chosen = bag[random(0, bag.length - 1)];
+
+    setDrawn(chosen);
+    subtractToken(chosen);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      {players.map((color) => {
+        return (
+          <div key={color}>
+            <p>{color}</p>
+            <button
+              onClick={() => {
+                addToken(color);
+              }}
+            >
+              +1
+            </button>
+            <span>{tokens[color]}</span>
+          </div>
+        );
+      })}
+      <div>
+        <button
+          onClick={() => {
+            setTokens(startingTokens);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        <p>Drawn: {drawn}</p>
+        <button onClick={drawRandom}>Draw Random</button>
+      </div>
+      <div>
+        <pre>{JSON.stringify(filterValue(tokens, 0), null, 2)}</pre>
+      </div>
     </div>
   );
 }
